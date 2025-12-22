4d14443dfdf6769e7c9832d2666d0e34fcc232c6
Apurba Bhadra <apurbabhadra@Apurbas-MacBook-Air.local>
2025-12-23 00:27:43 +0530

    multer issue solved

--- src/routes/admin/intersetsRoutes.ts
+++ src/routes/admin/intersetsRoutes.ts
@@ -6,9 +6,9 @@ import { multerErrorHandler } from '../../middleware/multerErrorhandler';
 
 const router=Router();
 
-router.post('/create',requireAuth,requireAdmin,createUploader('interests','image'),multerErrorHandler,interestController.CreateInterests)
+// router.post('/create',requireAuth,requireAdmin,createUploader('interests','image'),multerErrorHandler,interestController.CreateInterests)
 router.post("/getAll",requireAuth,requireAdmin,interestController.getAllInterests)
-router.patch("/update/:interestId",requireAuth,requireAdmin,createUploader('interests','image'),multerErrorHandler,interestController.updateInterests)
+// router.patch("/update/:interestId",requireAuth,requireAdmin,createUploader('interests','image'),multerErrorHandler,interestController.updateInterests)
 router.delete("/delete/:interestId",requireAuth,requireAdmin,interestController.deleteInterest)
 router.patch('/statusUpdate/:interestId',requireAuth,requireAdmin,interestController.statusChange)
 router.get("/getInterest/:interestId",requireAuth,requireAdmin,interestController.getInterest)
--- src/routes/user/profileRoutes.ts
+++ src/routes/user/profileRoutes.ts
@@ -8,7 +8,7 @@ import * as interestController from "../../controllers/admin/interestController"
 const router = Router();
 
 router.get('/getProfile', requireAuth, profileController.getProfile);
-router.patch('/updateProfile', requireAuth,createUploader('users','profilePicture'),multerErrorHandler,  profileController.updateProfile);
+// router.patch('/updateProfile', requireAuth,createUploader('users','profilePicture'),multerErrorHandler,  profileController.updateProfile);
 
 router.patch('/setInterests', requireAuth, profileController.setInterests);
 router.patch('/changePassword', requireAuth, profileController.changePassword);
